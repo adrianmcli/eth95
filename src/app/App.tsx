9799c5d8e009e985f6e7469576fd5fc1609f8548
Adrian Li <li.adrianmc@gmail.com>
2020-05-18 06:45:50 -0400

    make build work w/ parcel webapp

---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default () => (
+  <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+  </>
+);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
-const chalk = require("chalk");
-const clear = require("clear");
-const figlet = require("figlet");
-const path = require("path");
-const program = require("commander");
+import chalk from "chalk";
+import clear from "clear";
+import figlet from "figlet";
+import path from "path";
+import commander from "commander";
+import express from "express";
 
 clear();
 console.log(
@@ -17,11 +18,35 @@ console.log(
   ),
 );
 
-program
+commander
   .version("0.0.1")
   .description("A GUI for controlling your Ethereum dapp")
   .parse(process.argv);
 
-if (!process.argv.slice(2).length) {
-  program.outputHelp();
-}
+// if (!process.argv.slice(2).length) {
+//   program.outputHelp();
+// }
+
+const main = async () => {
+  const Bundler = require("parcel-bundler");
+
+  const parcelOptions = {
+    entryFiles: [path.join(__dirname, "./app/index.html")],
+    logLevel: 1,
+  };
+  const app: express.Application = express();
+
+  app.use("/api", function (req, res, next) {
+    console.log("wooooo weeeeeee");
+    res.json({ ping: "pong" });
+  });
+
+  const bundler = new Bundler(parcelOptions.entryFiles, parcelOptions);
+  app.use("/", bundler.middleware());
+
+  app.listen(3000, function () {
+    console.log("Server listening on port 3000.");
+  });
+};
+
+main();
