b76e524b0c50800e08b171eb608aec9842cc3a50
Adrian Li <li.adrianmc@gmail.com>
2020-05-18 07:34:58 -0400

    websockets now working

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 
+// console.log(`ws://${window.location.host}/ws`)
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// Connection opened
+socket.addEventListener("open", function (event) {
+  socket.send("Connection opened!");
+});
+
+// Listen for messages
+socket.addEventListener("message", function (event) {
+  console.log("Message from server ", event.data);
+});
+
 export default () => (
   <>
     <h1>Welcome to React Parcel Micro App!</h1>
     <p>Hard to get more minimal than this React app.</p>
+    <button
+      onClick={() =>
+        fetch("/api")
+          .then((x) => x.json())
+          .then(console.log)
+      }
+    >
+      click
+    </button>
+    <button onClick={() => socket.send("hey there!")}>send msg</button>
   </>
 );
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import figlet from "figlet";
 import path from "path";
 import commander from "commander";
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 clear();
 console.log(
@@ -45,7 +47,27 @@ const main = async () => {
   app.use("/", bundler.middleware());
 
   const port = 3000;
-  app.listen(port, function () {
+
+  // Do Websocket stuff
+  const server = http.createServer(app);
+  const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+
+  server.on("upgrade", function (request, socket, head) {
+    wss.handleUpgrade(request, socket, head, function (ws) {
+      wss.emit("connection", ws, request);
+    });
+  });
+
+  wss.on("connection", function (ws, request) {
+    ws.on("message", function (message) {
+      console.log(`Received message from client: ${message}`);
+    });
+    ws.on("close", function () {
+      console.log("Websocket connection closed.");
+    });
+  });
+
+  server.listen(port, function () {
     console.log(
       `Captain, you cockpit is ready for you at: http://localhost:${port}`,
     );
