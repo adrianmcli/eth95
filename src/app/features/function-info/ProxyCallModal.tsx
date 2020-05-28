7441651cacd7602ef110ef6de47e73c927f8ca17
Adrian Li <li.adrianmc@gmail.com>
2020-05-28 12:34:33 -0400

    remove direct call via dsproxy

--- src/app/features/function-info/ProxyCallModal.tsx
+++ src/app/features/function-info/ProxyCallModal.tsx
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Button, Panel } from "react95";
-import dappsys from "@studydefi/money-legos/dappsys";
+import { Panel } from "react95";
 
 import { ModalContainer, ModalHeader, ModalContent } from "../common/Modal";
-import ProxyAddress from "../../containers/ProxyAddress";
 import { ethers } from "ethers";
-import Input from "../common/Input";
-import Signers from "../../containers/Signers";
-import OutputLog from "../../containers/OutputLog";
 
 const DataPanel = styled(Panel)`
   padding: 1rem;
@@ -20,22 +15,8 @@ const DataPanel = styled(Panel)`
   overflow-y: auto;
 `;
 
-const FooterPanel = styled(Panel)`
-  padding: 0.1rem 0.25rem;
-  width: 100%;
-  font-size: 14px;
-  white-space: nowrap;
-  overflow: hidden;
-  line-height: 20px;
-  margin-bottom: 1rem;
-`;
-
 const ProxyCallModal = ({ closeModal, args, types, inputs, opts }) => {
-  const { signer } = Signers.useContainer();
-  const { proxyAddress } = ProxyAddress.useContainer();
-  const { addLogItem } = OutputLog.useContainer();
   const [encoded, setEncoded] = useState("");
-  const [targetAddress, setTargetAddress] = useState("");
   const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
@@ -61,28 +42,6 @@ const ProxyCallModal = ({ closeModal, args, types, inputs, opts }) => {
     }
   }, [args, types, inputs]);
 
-  const callFunction = async () => {
-    const instance = new ethers.Contract(
-      proxyAddress,
-      dappsys.dsProxy.abi,
-      signer,
-    );
-
-    const tx = await instance.execute(targetAddress, encoded, opts);
-    addLogItem(`tx.hash: ${tx.hash}`);
-    await tx.wait();
-    addLogItem(`tx mined: ${tx.hash}`);
-  };
-
-  const handleSubmit = async () => {
-    try {
-      await callFunction();
-    } catch (error) {
-      console.error(error);
-      addLogItem(`Error: ${error.message}`);
-    }
-  };
-
   return (
     <ModalContainer>
       <ModalHeader onCloseClick={closeModal} label="Submit via Proxy" />
@@ -105,21 +64,6 @@ const ProxyCallModal = ({ closeModal, args, types, inputs, opts }) => {
         </DataPanel>
         <div style={{ marginTop: "1rem" }}>Encoded:</div>
         <DataPanel variant="well">{encoded}</DataPanel>
-        <div style={{ marginTop: "1rem" }}>Target address:</div>
-        <Input
-          value={targetAddress}
-          onChange={(e) => setTargetAddress(e.target.value)}
-        />
-        <div style={{ marginTop: "1rem" }}>Proxy address:</div>
-        <FooterPanel variant="well">
-          {proxyAddress || "No proxy specified, cannot execute transaction"}
-        </FooterPanel>
-        <Button
-          disabled={!proxyAddress || !targetAddress || hasError}
-          onClick={handleSubmit}
-        >
-          Submit via Proxy
-        </Button>
       </ModalContent>
     </ModalContainer>
   );
